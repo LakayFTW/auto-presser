efec62f136b7f3867ae8a54b551a65fc84443b05
LakayFTW <MilchmannLetsPlay@gmail.com>
2023-07-20 18:50:51 +0200

    writing key is now asynchronous and can be stopped over ui

--- clicker.py
+++ clicker.py
@@ -1,39 +1,52 @@
 import keyboard
 import PySimpleGUI as sg
+from threading import Thread
 
-initial_text = "None"
-window_title = "Autoclicker v0.1"
+def write_key(key):
+    while start:
+        keyboard.write(f'{key}', delay=0.01)
 
-layout = [
-    [
-        sg.Button("Detect Keypress", border_width=0), sg.Text(initial_text, key='buttonPressed', size=30),
-    ],
-    [
-        sg.HSeparator()
-    ],
-    [
-        sg.Button("Start", border_width=0), sg.Button("Stop", border_width=0), sg.Button("CLOSE", border_width=0)
+def main():
+    global start
+    start = False
+    initial_text = "None"
+    window_title = "Autoclicker v0.1"
+    layout = [
+        [
+            sg.Button("Detect Keypress", border_width=0), sg.Text(initial_text, key='buttonPressed', size=30),
+        ],
+        [
+            sg.HSeparator()
+        ],
+        [
+            sg.Button("Start", border_width=0), sg.Button("Stop", border_width=0), sg.Button("CLOSE", border_width=0)
+        ]
     ]
-]
-sg.theme('DarkAmber')
 
-window = sg.Window(window_title, layout, size=(200, 200))
+    sg.theme('DarkAmber')
+    window = sg.Window(window_title, layout, size=(200, 200))
 
-while True:
-    event, values = window.read()
-    if event == "CLOSE" or event == sg.WIN_CLOSED:
-        break
-    if event == "Detect Keypress":
-        pressedKey = keyboard.read_key()
-        window['buttonPressed'].update(pressedKey)
-    if event == "Start" or keyboard.is_pressed('shift+space'):
-        print("started")
-        if pressedKey == None:
+    while True:
+        event, values = window.read()
+        if event == "CLOSE" or event == sg.WIN_CLOSED:
+            break
+        if event == "Detect Keypress":
+            pressedKey = keyboard.read_key()
+            window['buttonPressed'].update(pressedKey)
+        if event == "Start" or keyboard.is_pressed('shift+space'):
+            if pressedKey == None:
+                continue
+            start = True
+            # keyboard.write(f'{pressedKey}', delay=0.01)
+            thread = Thread(target=write_key, args=(pressedKey))
+            thread.start()
+        if event == "Stop" or keyboard.is_pressed('shift+a'):
+            start = False
+            thread.join()
             continue
-        while True:
-            keyboard.write(f'{pressedKey}', delay=0.01)
-            if event == "Stop" or keyboard.is_pressed('shift+a'):
-                print("stopped")
-                break
 
-window.close()
+    window.close()
+
+t1 = Thread(target=main)
+t1.start()
+t1.join()
